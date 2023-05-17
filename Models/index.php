bce34d34d0430df968c68132c2eeff4a29f72187
MarioBlasi <mblasi509@gmail.com>
2023-05-17 15:15:12 +0100

    $this->wheels = $wheels;

--- Models/index.php
+++ Models/index.php
@@ -1,26 +1,43 @@
 <?php
-// Classe Computer (parent class)
+  // Classe Computer (parent class)
   class Computer{
+    public $wheels;
 
+    public function __construct($wheels)
+    {
+
+      $this->wheels = $wheels;
+    }
+
+    public function componente() 
+    {
+      return 'porca troia';
+    }
   }
 
-// Classe Desktop che estende Computer
-class Desktop{
+  // Classe Desktop che estende Computer 
+  // classe figlio questa classe puo modificare il genotore USANDO EXTENDS
+  class Desktop{
 
-}
-// Classe Laptop che estende Computer
-class Laptop {
+  }
+  // Classe Laptop che estende Computer classe figlio 
+  //classe figlio questa classe puo modificare il genotore USANDO EXTENDS
+  class Laptop {
 
-}
-// Creazione degli oggetti
+  }
+  // Creazione degli oggetti
+  $desktop = new Desktop("Desktop");
+  $laptop = new Laptop("Laptop");
 
-// Array di oggetti
-$computers = [$desktop, $laptop];
+  var_dump($desktop);
+  var_dump($laptop);
+  // Array di oggetti
+  $computers = [$desktop, $laptop];
 
 
-// Stampo le card utilizzando Bootstrap
+  // Stampo le card utilizzando Bootstrap
 
-echo ecc ecc 
+  //echo ecc ecc 
 ?>
 
 <!doctype html>
@@ -43,7 +60,20 @@ echo ecc ecc
     <h1>php-oop-2</h1>
   </header>
   <main>
-
+      <div class="container mt-5 shadow">
+        <div class="row">
+          <div class="col p-3">
+            <div class="card">
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus tempora corrupti laborum ut maiores labore! Enim, quod nihil vel ab deserunt est dolor. Accusantium rem, magnam cupiditate ratione ad adipisci.</p>
+            </div>
+          </div>
+          <div class="col p-3">
+            <div class="card">
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus tempora corrupti laborum ut maiores labore! Enim, quod nihil vel ab deserunt est dolor. Accusantium rem, magnam cupiditate ratione ad adipisci.</p>
+            </div>
+          </div>
+        </div>
+      </div>
   </main>
   <footer>
     <!-- place footer here -->
