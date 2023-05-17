0786089b55e07d309e2e911120f116698927ef3d
MarioBlasi <mblasi509@gmail.com>
2023-05-17 17:52:28 +0100

    require DIR

--- index.php
+++ index.php
@@ -1,24 +1,17 @@
 <?php
+
   require __DIR__ . './Models/Computer.php';
   require __DIR__ . './Models/Desktop.php';
-
   require __DIR__ . './Models/Laptop.php';
-  
-  
+
   // Creazione degli oggetti
   $desktop = new Desktop("Desktop");
   $laptop = new Laptop("Laptop");
-
-  // var_dump($desktop);
-  // var_dump($laptop);
-  
   // Array di oggetti
   $computers = [$desktop, $laptop];
-
   // Chiamata al metodo displayProductType() polimorfico
   $desktop->displayProductType(); // Stampa: "Il computer desktop si avvia..."
   $laptop->displayProductType(); // Stampa: "Il computer portatile si avvia..."
-
 ?>
 
 <!doctype html>
@@ -50,12 +43,41 @@
   <div class="container mt-3 ">
     <div class="row">
       <div class="col ">
-        <nav class="navbar navbar-light justify-content-between bg-dark text-light">
-          <a class="navbar-brand text-light">Navbar</a>
-          <form class="form-inline">
-            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-          </form>
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        <nav class="navbar navbar-expand-lg bg-light">
+          <div class="container-fluid">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Dropdown
+                  </a>
+                  <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">Action</a></li>
+                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                    <li><hr class="dropdown-divider"></li>
+                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                  </ul>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link disabled">Disabled</a>
+                </li>
+              </ul>
+              <form class="d-flex" role="search">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+              </form>
+            </div>
+          </div>
         </nav>
       </div>
     </div>
@@ -63,13 +85,13 @@
   <!-- nav -->
   <main>
     <div class="container mt-5">
-        <div class="row justify-content-md-center">
+        <div class="row row-cols-1 row-cols-md-2 justify-content-md-center">
             <div class="col p-3">
                 <div class="card">
                     <div class="card-body">
                         <img class="card-img" src="https://picsum.photos/600/300" alt="">
-                        <h4 class="card-title"><?php echo $desktop->displayProductType(); ?></h4>
-                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta saepe aperiam quae ut dicta neque, suscipit, illo consequatur autem commodi similique nihil quas animi dolores ipsum quos, excepturi dolorum aliquid?</p>
+                        <h4 class="card-title mt-4"><?php echo $desktop->displayProductType(); ?></h4>
+                        <p class="card-text mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta saepe aperiam quae ut dicta neque, suscipit, illo consequatur autem commodi similique nihil quas animi dolores ipsum quos, excepturi dolorum aliquid?</p>
                     </div>
                 </div>
             </div>
@@ -77,22 +99,21 @@
                 <div class="card">
                     <div class="card-body">
                         <img class="card-img" src="https://unsplash.it/600/300?image" alt="">
-                        <h4 class="card-title"><?php echo $laptop->displayProductType(); ?></h4>
-                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta saepe aperiam quae ut dicta neque, suscipit, illo consequatur autem commodi similique nihil quas animi dolores ipsum quos, excepturi dolorum aliquid?</p>
+                        <h4 class="card-title mt-4"><?php echo $laptop->displayProductType(); ?></h4>
+                        <p class="card-text mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta saepe aperiam quae ut dicta neque, suscipit, illo consequatur autem commodi similique nihil quas animi dolores ipsum quos, excepturi dolorum aliquid?</p>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-
   </main>
   <!-- main -->
   <footer>
-    <div class="container mt-5">
+    <div class="container mt-3">
       <div class="row">
         <div class="col">
-          <div class="card text-center bg-dark text-light">
-            <p> by Mario Blasi</p>
+          <div class="card text-center bg-dark text-light ">
+            <p class="mt-3"> by Mario Blasi</p>
           </div>
         </div>
       </div>
